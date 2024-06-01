e2c183c9dcaa6222eff1da52a4aa9b96441ae361
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-06-01 13:06:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 const btn=document.getElementById('btn')
 const selectInput=document.getElementById('colorSelect')
 
+// btn.addEventListener('click',()=>{
+// 	const selectedOption=document.querySelector(`#colorSelect option[value='${selectInput.value}']`)
+// 	if(selectedOption){
+// 		selectInput.removeChild(selectedOption)
+// 	} 
+// })
+
+// without touching index.html options abd without queryselector for options
 btn.addEventListener('click',()=>{
-	const selectedOption=document.querySelector(`#colorSelect option[value='${selectInput.value}']`)
-	if(selectedOption){
-		selectInput.removeChild(selectedOption)
-	} 
+	// console.log(selectInput.selectedIndex)
+	selectInput.remove(selectInput.selectedIndex)
 })
