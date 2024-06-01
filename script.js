aac532f54c71f03a2532f6a84ce8ea2db1ed3455
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-06-01 12:58:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const btn=document.getElementById('btn')
+const selectInput=document.getElementById('colorSelect')
+
+btn.addEventListener('click',()=>{
+	const selectedOption=document.querySelector(`#colorSelect option[value='${selectInput.value}']`)
+	if(selectedOption){
+		selectInput.removeChild(selectedOption)
+	}
+})
